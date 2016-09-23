9b7c084a074854568c552c89fa94bc93ad199976
alvarojoao <alvaro@silvino.me>
2016-09-23 17:56:09 -0300

    adicioanndo cors e adicionando melhorias em atendimento

---
+++ www/js/modules/atendimento/atendimento.service.js
@@ -0,0 +1,16 @@
+(function() {
+    angular
+    .module('myApp.atendimento')
+    .service('atendimentoWatson', AtendimentoWatson);
+
+    AtendimentoWatson.$inject = ['$http'];
+
+    function AtendimentoWatson($http) {
+        var vm = this;
+
+        vm.sendMessage =  function sendMessage(message) {
+            var params = {message: message};
+            return $http.post('/api',params);
+        };
+    }
+})();
